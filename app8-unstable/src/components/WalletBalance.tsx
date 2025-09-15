b9e58dd21e7c56a6bd338787f42fc89c3272e6f8
Ubuntu <codepen@codepen.qk1czzwt2chupc2qqtt3sndnqb.bx.internal.cloudapp.net>
2025-09-15 15:58:13 +0000

    Keep only ambrosia and .kiro directories

--- app8-unstable/src/components/WalletBalance.tsx
+++
@@ -1,9 +0,0 @@
-import { WalletBalanceDisplay } from './UnifiedWallet';
-
-interface WalletBalanceProps {
-  onNavigate?: (screen: string) => void;
-}
-
-export function WalletBalance(props: WalletBalanceProps) {
-  return <WalletBalanceDisplay {...props} />;
-}
